edde78318698228a75d8fcb639e6550280424289
Dan Kortschak <dan@kortschak.io>
2020-11-21 15:38:04 +1030

    travis: test on 386

--- .travis/script.d/test.sh
+++ .travis/script.d/test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Travis clobbers GOARCH env vars so this is necessary here.
+if [[ -n $FORCEGOARCH ]]; then
+	export GOARCH=$FORCEGOARCH
+fi
+
 go get -d -v ./...
 go build -v ./...
 go test $TAGS -v ./...
